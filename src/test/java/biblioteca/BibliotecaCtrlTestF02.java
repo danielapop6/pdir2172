aa39f26f3647c5e36be19eaeaa63d32215218537
danaa <danaaaapop@gmail.com>
2019-05-20 21:28:42 +0300

    lab3

---
+++ src/test/java/biblioteca/BibliotecaCtrlTestF02.java
@@ -0,0 +1,50 @@
+package biblioteca;
+
+import biblioteca.model.Carte;
+import biblioteca.repository.repoMock.CartiRepoMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaCtrlTestF02 {
+    CartiRepoMock cartiRepoMock;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+    @Test
+    public void cautaCarteInListaGoala() {
+        List<Carte> carti = new ArrayList<>();
+        cartiRepoMock = new CartiRepoMock(carti); // lista goala
+
+            List<Carte> cartiCautate = cartiRepoMock.cautaCarte("Enigma");
+            Assert.assertEquals(0,cartiCautate.size());
+    }
+
+    @Test
+    public void cautaCarteListaCuUnSingurElement() {
+        List<Carte> carti = new ArrayList<>();
+        carti.add(Carte.getCarteFromString("Povesti;Mihai Eminescu,Ion Caragiale,Ion Creanga;1973;Corint;povesti,povestiri"));
+        cartiRepoMock = new CartiRepoMock(carti);
+
+            List<Carte> cartiCautate = cartiRepoMock.cautaCarte("P");
+            Assert.assertEquals(1,cartiCautate.size());
+    }
+
+    @Test
+    public void cautaCarteListaCuMaiMulteElemente() {
+        cartiRepoMock = new CartiRepoMock();
+
+        List<Carte> cartiCautate = cartiRepoMock.cautaCarte("enigma");
+        Assert.assertEquals(1,cartiCautate.size());
+    }
+}
